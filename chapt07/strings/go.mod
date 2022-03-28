6fb3fa518eefc52e4f4ecd1933ee8cf2d0ebf16c
Dmitrii Kilishek <mentalclear@gmail.com>
2022-03-28 00:02:57 -0400

    Contunue with Converting a String to Runes

---
+++ chapt07/strings/go.mod
@@ -0,0 +1,3 @@
+module github.com/mentalclear-learningo/progo/chapt07/strings
+
+go 1.18
---
+++ chapt07/strings/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("Understanding the Dual Nature of Strings:")
+	dualStringNature()
+	fmt.Println("Euro sgin pitfall:")
+	euroSignPitfall()
+	fmt.Println("Converting a String to Runes:")
+	convertStringsToRunes()
+}
+
+func dualStringNature() {
+	var price string = "$48.95"
+	// var currency byte = price[0]
+	var currency string = string(price[0])
+	var amountString string = price[1:]
+	amount, parseErr := strconv.ParseFloat(amountString, 64)
+	fmt.Println("Currency byte:", currency, "Actual string:", string(currency))
+	if parseErr == nil {
+		fmt.Println("Amount:", amount)
+	} else {
+		fmt.Println("Parse Error:", parseErr)
+	}
+}
+
+func euroSignPitfall() {
+	var price string = "€48.95"
+	var currency string = string(price[0])
+	var amountString string = price[1:]
+	amount, parseErr := strconv.ParseFloat(amountString, 64)
+	fmt.Println("Length:", len(price))
+	fmt.Println("Currency:", currency)
+	if parseErr == nil {
+		fmt.Println("Amount:", amount)
+	} else {
+		fmt.Println("Parse Error:", parseErr)
+	}
+}
+
+func convertStringsToRunes() {
+	var price []rune = []rune("€48.95")
+	var currency string = string(price[0])
+	var amountString string = string(price[1:])
+	amount, parseErr := strconv.ParseFloat(amountString, 64)
+	fmt.Println("Length:", len(price))
+	fmt.Println("Currency:", currency)
+	if parseErr == nil {
+		fmt.Println("Amount:", amount)
+	} else {
+		fmt.Println("Parse Error:", parseErr)
+	}
+}
