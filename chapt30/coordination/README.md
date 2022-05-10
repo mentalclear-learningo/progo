7625154861d28ac133743077a784538368c7c477
Dmitrii Kilishek <mentalclear@gmail.com>
2022-05-10 08:20:32 -0400

    Complete Chapter 30 for now

---
+++ chapt30/coordination/README.md
@@ -0,0 +1,3 @@
+### Cahpter 30. Coordinating Goroutines
+
+Continue with this one later: Using a Read-Write Mutex
--- chapt30/coordination/main.go
+++ chapt30/coordination/main.go
@@ -1,12 +1,32 @@
 package main
 
+import (
+	"sync"
+	"time"
+)
+
+var waitGroup = sync.WaitGroup{}
+var mutex = sync.Mutex{}
+
 func doSum(count int, val *int) {
+	time.Sleep(time.Second)
+	mutex.Lock() // To have fewer mutex ops, use before loop
 	for i := 0; i < count; i++ {
+		//mutex.Lock() // Lock on variable change
 		*val++
+		//mutex.Unlock() // Unock after variable change
 	}
+	mutex.Unlock()
+	waitGroup.Done()
 }
+
 func main() {
 	counter := 0
-	doSum(5000, &counter)
+	numRoutines := 5
+	waitGroup.Add(numRoutines)
+	for i := 0; i < numRoutines; i++ {
+		go doSum(5000, &counter)
+	}
+	waitGroup.Wait()
 	Printfln("Total: %v", counter)
 }
